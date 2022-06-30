740225b3ffa1f9bac486e0d4f5abe93fb51ccf4e
YosephMekonnen <Yosef.mek12@gmail.com>
2022-06-30 22:19:05 +0300

    second commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 1
+*/
+int main(void)
+{
+char[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2,ch,sizeof(ch));
+return (1);
+}
+
