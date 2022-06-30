99fb5ad030de540a7e3ce39d0e6d7ae9e1017733
YosephMekonnen <Yosef.mek12@gmail.com>
2022-06-30 22:22:11 +0300

    second commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - main block
  * Return: 1
 */
 int main(void)
 {
-char[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2,ch,sizeof(ch));
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, ch, sizeof(ch));
 return (1);
 }
 
