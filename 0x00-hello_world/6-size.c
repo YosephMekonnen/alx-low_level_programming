bd10f95d90c8f1980faae145c6031feb72709251
YosephMekonnen <Yosef.mek12@gmail.com>
2022-06-30 22:04:51 +0300

    second commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %i bytes \n",sizeof(char));
-printf("Size of a int: %i bytes \n",sizeof(int));
-printf("Size of a long int: %i bytes \n",sizeof(long int));
-printf("Size of a long long int: %i bytes \n",sizeof(long long int));
-printf("Size of a float: %i bytes \n",sizeof(float));
+printf("Size of a char: %i bytes \n", sizeof(char));
+printf("Size of a int: %i bytes \n", sizeof(int));
+printf("Size of a long int: %i bytes \n", sizeof(long int));
+printf("Size of a long long int: %i bytes \n", sizeof(long long int));
+printf("Size of a float: %i bytes \n", sizeof(float));
 return (0);
 }
